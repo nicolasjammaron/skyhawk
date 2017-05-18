1215d5070697699dd942fcce479aed399a28078b
Nicolas Jammaron <nicolas.jammaron@ensimag.grenoble-inp.fr>
2017-05-18 19:46:50 +0200

    premier commit

---
+++ beginning/test1.py
@@ -0,0 +1,9 @@
+import GDAX as gdax
+import json
+import time
+
+publicCLient = gdax.PublicClient(product_id="BTC-EUR")
+orderbook = publicCLient.getProductOrderBook(level=1)
+infos = (orderbook['asks'][0][0],orderbook['bids'][0][1])
+print(infos)
+
