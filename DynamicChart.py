7fc1830b0f32c6bf42422fe3058e1170cff1aa5f
Nicolas Jammaron <nicolas.jammaron@ensimag.grenoble-inp.fr>
2017-06-09 15:37:49 +0200

    ménage

--- DynamicChart.py
+++ DynamicChart.py
@@ -11,6 +11,8 @@ k = krakenex.API()
 s = sched.scheduler(time.time,time.sleep)
 prices = []
 xtime = []
+
+
 def update_chart():
     print("new minute")
     listPrices = k.query_public("OHLC", {'since':time.time()-59, 'pair': 'XXBTZEUR', 'interval': 1})
@@ -23,5 +25,8 @@ listPrices = k.query_public("OHLC", {'since':time.time()-59, 'pair': 'XXBTZEUR',
 prices.append(listPrices['result']['XXBTZEUR'][0][4])
 xtime.append(0)
 print(prices)
+
 s.enter(60,1,update_chart)
+
+
 s.run()
--- OrderBookHandling/OrderbookPersonnalisedClass.py
+++
@@ -1,8 +0,0 @@
-import GDAX
-
-class OrderBookPersonnalised:
-
-
-    def __init__(self,orderbook,time):
-        self.orderbook = orderbook
-        self.time = time
